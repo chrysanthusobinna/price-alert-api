29d5397b26497dc24a6d7bc9d9f9349a306c89ec
chrysanthusobinna <chrysanthusobinna@gmail.com>
2025-05-01 16:05:53 +0100

    feat: enable scheduling and load product data from PricetrackerApplication

--- src/main/java/com/pricetracker/pricetracker/PricetrackerApplication.java
+++ src/main/java/com/pricetracker/pricetracker/PricetrackerApplication.java
@@ -2,12 +2,27 @@ package com.pricetracker.pricetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import com.pricetracker.pricetracker.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class PricetrackerApplication {
+@EnableScheduling
+@EnableWebMvc
+public class PricetrackerApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductService productService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PricetrackerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Load all products from JSON at startup
+		productService.loadAllProducts();
+	}
 }
